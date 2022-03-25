a7d8887b6f4ceda74502acb91eb43ed3b0b8ef27
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-03-25 10:04:36 +0100

    feat: function to execute login or give errors when login fails

--- classes/User.php
+++ classes/User.php
@@ -1,18 +1,21 @@
 <?php
 
 include_once(__DIR__ . "/Db.php");
-class User{
+class User
+{
         private $firstname;
         private $lastname;
         private $username;
         private $email;
         private $password;
-     
-        public function getFirstname(){
+
+        public function getFirstname()
+        {
                 return $this->firstname;
         }
 
-        public function setFirstname($firstname){
+        public function setFirstname($firstname)
+        {
                 if (empty($firstname)) {
                         throw new Exception("firstname cant be empty");
                 }
@@ -21,11 +24,13 @@ class User{
                 return $this;
         }
 
-        public function getLastname(){
+        public function getLastname()
+        {
                 return $this->lastname;
         }
 
-        public function setLastname($lastname){
+        public function setLastname($lastname)
+        {
                 if (empty($lastname)) {
                         throw new Exception("lastname cant be empty");
                 }
@@ -34,28 +39,31 @@ class User{
                 return $this;
         }
 
-        public function getEmail(){
+        public function getEmail()
+        {
                 return $this->email;
         }
 
-        public function setEmail($email){
+        public function setEmail($email)
+        {
                 if (!str_ends_with($email, '@student.thomasmore.be')) {
                         if (!str_ends_with($email, '@thomasmore.be')) {
                                 throw new Exception("email has to end with @student.thomasmore.be or @thomasmore.be");
                         }
-                }
-                else if (empty($email)) {
+                } else if (empty($email)) {
                         throw new Exception("email cant be empty");
                 }
                 $this->email = $email;
                 return $this;
         }
 
-        public function getPassword(){
+        public function getPassword()
+        {
                 return $this->password;
         }
 
-        public function setPassword($password){
+        public function setPassword($password)
+        {
                 if (strlen($password) < 5) {
                         throw new Exception("Passwords must be longer than 5 characters.");
                 }
@@ -63,7 +71,30 @@ class User{
                 return $this;
         }
 
-        public static function getAll(){
+        public function canLogin()
+        {
+                $conn = Db::getInstance();
+                $statement = $conn->prepare("select * from users where email = :email;");
+                $statement->bindValue(':email', $this->email);
+                $statement->execute();
+                $user = ($statement->fetch());
+                $hash = $user["password"];
+
+                if (!$user) {
+                        echo "user does not exist";
+                        return false;
+                }
+                if (password_verify($this->password, $hash)) {
+                        echo "you're logged in";
+                        return true;
+                } else {
+                        echo "wrong password";
+                        return false;
+                }
+        }
+
+        public static function getAll()
+        {
                 $conn = Db::getInstance();
                 $sql = "SELECT * FROM users";
                 $statement = $conn->prepare($sql);
@@ -72,9 +103,10 @@ class User{
                 return $result;
         }
 
-        public function register() {
+        public function register()
+        {
                 $options = [
-                    'cost' => 12
+                        'cost' => 12
                 ];
                 $password = password_hash($this->password, PASSWORD_BCRYPT, $options);
                 $conn = Db::getInstance();
@@ -86,28 +118,28 @@ class User{
                 return $statement->execute();
         }
 
-        public function canRegister($password, $password2) {
+        public function canRegister($password, $password2)
+        {
                 $conn = Db::getInstance();
                 $statement = $conn->prepare("select * from users where email = :email");
-                $statement -> bindValue(":email", $this -> email);
-                $statement -> execute();
+                $statement->bindValue(":email", $this->email);
+                $statement->execute();
                 $user = ($statement->fetch());
-                if(!$user){
-                        if($password == $password2){
+                if (!$user) {
+                        if ($password == $password2) {
                                 return true;
-                        }
-                        else{
+                        } else {
                                 throw new Exception("wachtwoorden komen niet overeen");
                                 return false;
                         }
-                }
-                else{
+                } else {
                         throw new Exception("gebruiker bestaat al");
                         return false;
                 }
         }
 
-        public function __toString(){
-                return $this->firstname . " " . $this->lastname ." ". $this->email ." ". $this->email;
+        public function __toString()
+        {
+                return $this->firstname . " " . $this->lastname . " " . $this->email . " " . $this->email;
         }
-    }
+}
