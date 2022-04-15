d9463ad92f2ca51c5a2bd01387a7267185b51b15
unknown <r0818162@student.thomsmore.be>
2022-04-15 13:40:19 +0200

    style: basic header styling

--- nav.php
+++ nav.php
@@ -1,14 +1,20 @@
-<?php 
+
+
+<nav>
+    <span>Brandish</span>
+
+    <?php 
     if (isset($_SESSION['user'])) {
         $sessionId = $_SESSION['user'];
         $user = User::getUserFromEmail($sessionId);
         echo "Welkom ".$user["firstname"];
+        echo "<div class='nav_right'><a class='nav_btn' href='logout.php'>logout</a></div>";
+    }
+    else{
+        echo "<div class='nav_right'><a class='nav_btn' href='login.php'>Inloggen</a> ";
+        echo "<a class='nav_btn' href='register.php'>Aanmelden</a></div>";
     }
     
-
 ?>
 
-<nav class="navbar">
-    <a class="navbar__btn" href="index.php">Home</a>
-    <a class="navbar__btn" href="logout.php">logout</a>
 </nav>
