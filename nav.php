3b95a26a82622fbb442aa0b4efe7906137196a52
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-04-19 14:10:55 +0200

    feat: add edit profile button to nav

--- nav.php
+++ nav.php
@@ -1,20 +1,18 @@
-
-
 <nav>
     <span>Brandish</span>
 
-    <?php 
+    <?php
     if (isset($_SESSION['user'])) {
         $sessionId = $_SESSION['user'];
         $user = User::getUserFromEmail($sessionId);
-        echo "Welkom ".$user["firstname"];
+        // echo "Welkom ".$user["firstname"];
         echo "<div class='nav_right'><a class='nav_btn' href='logout.php'>logout</a></div>";
-    }
-    else{
+        echo "<div class='nav_right'><a class='nav_btn' href='usersettings.php'>edit profile</a></div>";
+    } else {
         echo "<div class='nav_right'><a class='nav_btn' href='login.php'>Inloggen</a> ";
         echo "<a class='nav_btn' href='register.php'>Aanmelden</a></div>";
     }
-    
-?>
+
+    ?>
 
 </nav>
--- usersettings.php
+++ usersettings.php
@@ -1,72 +1,68 @@
 <?php
 
-    include_once("bootstrap.php");
+include_once("bootstrap.php");
 
-    session_start();
-    if(!isset($_SESSION['user'])) {
-        header('location: login.php');
-    } else{
-        $user = new User();
-        $sessionId = $_SESSION['user'];
-        $userData = User::getUserFromEmail($sessionId);
-    }
+session_start();
+if (!isset($_SESSION['user'])) {
+    header('location: login.php');
+} else {
+    $user = new User();
+    $sessionId = $_SESSION['user'];
+    $userData = User::getUserFromEmail($sessionId);
+}
 
-    if(!empty($_POST['updateImage'])) {
-            
-            
-        $imageName = $_FILES['userImage']['name'];
-        $fileType  = $_FILES['userImage']['type'];
-        $fileSize  = $_FILES['userImage']['size'];
-        $fileTmpName = $_FILES['userImage']['tmp_name'];
-        $fileError = $_FILES['userImage']['error'];
+if (!empty($_POST['updateImage'])) {
 
-        $fileData = explode('/',$fileType);
-        $fileExtension = $fileData[count($fileData)-1];
 
-        if($fileExtension == 'jpg' || $fileExtension == 'jpeg' || $fileExtension == 'png'){
-            //check if file is correct type
-            //check file size
-            if($fileSize < 5000000){
+    $imageName = $_FILES['userImage']['name'];
+    $fileType  = $_FILES['userImage']['type'];
+    $fileSize  = $_FILES['userImage']['size'];
+    $fileTmpName = $_FILES['userImage']['tmp_name'];
+    $fileError = $_FILES['userImage']['error'];
 
-                $fileNewName = "images/profile_pictures/".basename($imageName);
-                $uploaded = move_uploaded_file($fileTmpName,$fileNewName);
+    $fileData = explode('/', $fileType);
+    $fileExtension = $fileData[count($fileData) - 1];
 
-                if($uploaded){
-                    $profilePicture = basename($imageName);
-                    $user->setProfilePicture($profilePicture);
-                    $user->updateProfilePicture($profilePicture, $sessionId);
-                }
+    if ($fileExtension == 'jpg' || $fileExtension == 'jpeg' || $fileExtension == 'png') {
+        //check if file is correct type
+        //check file size
+        if ($fileSize < 5000000) {
 
-            }else{
-                echo("too big");
+            $fileNewName = "images/profile_pictures/" . basename($imageName);
+            $uploaded = move_uploaded_file($fileTmpName, $fileNewName);
+
+            if ($uploaded) {
+                $profilePicture = basename($imageName);
+                $user->setProfilePicture($profilePicture);
+                $user->updateProfilePicture($profilePicture, $sessionId);
             }
-        }else{
-            echo("no");
+        } else {
+            echo ("too big");
         }
-        
-                
+    } else {
+        echo ("no");
     }
+}
 
 
-    if(!empty($_POST['update'])) {
-        try {
-            $user->setFirstName($firstname = $_POST['updateFirstName']);
-            $user->setLastname($_POST['updateLastName']);
-            $user->setEmail($_POST['updateEmail']);
+if (!empty($_POST['update'])) {
+    try {
+        $user->setFirstName($firstname = $_POST['updateFirstName']);
+        $user->setLastname($_POST['updateLastName']);
+        $user->setEmail($_POST['updateEmail']);
 
-            //$user->setProfilePicture($_FILES['updateImage']);
-            $user->updateUser();
-            
-            $userData = User::getUserFromEmail($sessionId);
-            
-            } catch (\Throwable $th) {
-                $error = true;
-            }
-            
+        //$user->setProfilePicture($_FILES['updateImage']);
+        $user->updateUser();
+
+        $userData = User::getUserFromEmail($sessionId);
+    } catch (\Throwable $th) {
+        $error = true;
     }
-    
+}
 
-?><!DOCTYPE html>
+
+?>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -79,34 +75,34 @@
 
 <body>
 
-<header>
-        <?php include('nav.php');?>
-</header>
+    <header>
+        <?php include('nav.php'); ?>
+    </header>
 
     <?php if (!empty($error)) : ?>
         <div>Sorry, Something went wrong</div>
     <?php endif; ?>
 
     <form action="" method="POST" enctype="multipart/form-data">
-    
+
         <div>
             <label>Profile picture</label>
             <img class="profilePicture" src="images/profile_pictures/<?php echo $userData['profilepicture']; ?>" alt="Profile picture">
             <input type="file" id="userImage" name="userImage" value=""><br>
-            
+
         </div>
         <input type="submit" name="updateImage" value="Update profile picture">
     </form>
-    
+
     <form action="" method="post">
         <label>Email address</label>
-        <input type="email" name="updateEmail" value="<?php echo htmlspecialchars($userData['email']);?>"><br>
+        <input type="email" name="updateEmail" value="<?php echo htmlspecialchars($userData['email']); ?>"><br>
 
         <label>First Name</label>
-        <input type="text" name="updateFirstName" value="<?php echo htmlspecialchars($userData['firstname']);?>"><br>
+        <input type="text" name="updateFirstName" value="<?php echo htmlspecialchars($userData['firstname']); ?>"><br>
 
         <label>Last Name</label>
-        <input type="text" name="updateLastName" value="<?php echo htmlspecialchars($userData['lastname']);?>"><br>
+        <input type="text" name="updateLastName" value="<?php echo htmlspecialchars($userData['lastname']); ?>"><br>
 
         <input type="submit" name="update" value="Update gegevens">
     </form>
