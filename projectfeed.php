a26b4eda2efae3ce4aad21606a81e9b6e03b8aa6
Alejandro De Wolf <dewolf.alejandro@gmail.com>
2022-05-01 16:57:14 +0200

    fix: delete line of code that caused an error

--- projectfeed.php
+++ projectfeed.php
@@ -1,22 +1,24 @@
 <?php
-    include_once("bootstrap.php");
+include_once("bootstrap.php");
 
-    session_start();
-    if (!isset($_SESSION['user'])) {
-        header('location: login.php');
-    } else {
-        $user = new User();
-        $sessionId = $_SESSION['user'];
-        $userData = User::getUserFromEmail($sessionId);
-    }
+session_start();
+if (!isset($_SESSION['user'])) {
+    header('location: login.php');
+} else {
+    $user = new User();
+    $sessionId = $_SESSION['user'];
+    $userData = User::getUserFromEmail($sessionId);
+}
 
-    $post = new Post;
-    $allPosts = $post->getAllPostsLimit();
-    //var_dump($allPosts);
-            
+$post = new Post;
+$allPosts = $post->getAllPostsLimit();
+//var_dump($allPosts);
 
-?><!DOCTYPE html>
+
+?>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -24,41 +26,42 @@
     <title>Creative Minds</title>
     <link rel="stylesheet" href="styles/style.css">
 </head>
+
 <body>
 
     <header>
         <?php include('nav.php'); ?>
     </header>
-    
+
     <div class="posts">
-        <?php foreach($allPosts as $p): ?>
+        <?php foreach ($allPosts as $p) : ?>
 
-        <div class="post">
+            <div class="post">
 
-            <div class="post_head">
-                <img class="post_image" src="<?php echo $p['imgpath']; ?>" alt="">
+                <div class="post_head">
+                    <img class="post_image" src="<?php echo $p['imgpath']; ?>" alt="">
+                </div>
+
+                <a href="userdata.php?id=<?php echo $p['userid'] ?>" class="post_userinfo">
+                    <img class="profilePicture_small" src="images/profile_pictures/<?php echo $post->getUserByPostId($p['id'])['profilepicture'] ?>" alt="">
+                    <p class="post_username"><?php echo $post->getUserByPostId($p['id'])['firstname'] . " " . $post->getUserByPostId($p['id'])['lastname']; ?></p>
+                </a>
+
+                <div class="post_content">
+                    <p><?php echo $p['title']; ?></p>
+                    <p><?php echo $p['description']; ?></p>
+                </div>
             </div>
-            <?php echo $p['user.id'];?>
-
-            <a href="userdata.php?id=<?php echo $p['userid']?>" class="post_userinfo">
-                <img class="profilePicture_small" src="images/profile_pictures/<?php echo $post->getUserByPostId($p['id'])['profilepicture'] ?>" alt="">
-                <p class="post_username"><?php echo $post->getUserByPostId($p['id'])['firstname'] . " " . $post->getUserByPostId($p['id'])['lastname']; ?></p>
-            </a>
-
-            <div class="post_content">
-                <p><?php echo $p['title']; ?></p>
-                <p><?php echo $p['description']; ?></p>
-            </div>
-        </div>
         <?php endforeach; ?>
     </div>
 
     <div class="pages">
-       <?php for ($pages=1; $pages <= $total_pages ; $pages++):?>
-            <a href='<?php echo "?page=$pages"; ?>' class="links"><?php  echo $pages; ?></a>
-        <?php endfor; ?> 
+        <?php for ($pages = 1; $pages <= $total_pages; $pages++) : ?>
+            <a href='<?php echo "?page=$pages"; ?>' class="links"><?php echo $pages; ?></a>
+        <?php endfor; ?>
     </div>
-    
+
 
 </body>
+
 </html>
