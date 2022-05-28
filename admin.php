9265327b7be81a1594dba3affe18d12835191c63
Alejandro De Wolf <dewolf.alejandro@gmail.com>
2022-05-28 13:45:29 +0200

    feat: admins now have the right to send warning or ban messages to other users

---
+++ admin.php
@@ -0,0 +1,20 @@
+<?php
+
+
+include_once("classes/Db.php");
+
+$conn = Db::getInstance();
+
+if (isset($_GET['ban'])) {
+    $id = $_GET['ban'];
+    echo "You have banned <b>".$id."</b>";
+    $sql = "UPDATE users SET warning = '2' WHERE id = '$id'";
+    $result = $conn->query($sql);
+}
+if (isset($_GET['warn'])) {
+    $id = $_GET['warn'];
+    echo "You have sent a warning to <b>".$id."</b>";
+    $sql = "UPDATE users SET warning = '1' WHERE id = '$id'";
+    $result = $conn->query($sql);
+}
+header('Location: usersettings.php');
